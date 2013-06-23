be2d0e4f4ecc2e949ad4bbb0cea3980f5ae40b49
fkiss <fkiss@redhat.com>
2013-06-23 18:40:45 +0200

    update in search

--- src/main/java/cz/muni/fi/pv243/cookbook/finders/RecipeFinder.java
+++ src/main/java/cz/muni/fi/pv243/cookbook/finders/RecipeFinder.java
@@ -11,6 +11,7 @@ import javax.inject.Named;
 
 import cz.muni.fi.pv243.cookbook.DAO.RecipeDao;
 import cz.muni.fi.pv243.cookbook.DAO.UserDao;
+import cz.muni.fi.pv243.cookbook.categories.FoodCategory;
 import cz.muni.fi.pv243.cookbook.model.Ingredient;
 import cz.muni.fi.pv243.cookbook.model.Recipe;
 import cz.muni.fi.pv243.cookbook.model.User;
@@ -25,55 +26,67 @@ import java.util.Map.Entry;
 @RequestScoped
 public class RecipeFinder {
 
-		@Inject
-	private RecipeDao recipeDao;
+    @Inject
+    private RecipeDao recipeDao;
+    
+    @Inject
+    private IngredientDao ingredientDao;
+    
+    @Inject
+    UserDao userDao;
+    private List<Recipe> foundRecipes = new ArrayList<Recipe>();
+
+    public List<Recipe> getFoundRecipes() {
+        return foundRecipes;
+    }
+
+    public void setFoundRecipes(List<Recipe> foundRecipes) {
+        this.foundRecipes = foundRecipes;
+    }
+
+    public RecipeFinder() {
+        Recipe recipe = new Recipe();
         
-                    @Inject
-                    private IngredientDao ingredientDao;
-                    
-                    @Inject UserDao userDao;
-                            
-	public RecipeFinder() {
-	}
+        this.foundRecipes.add(recipe);
+    }
 
-	public List<Recipe> searchEverywhere(String attributes) {
-		
-		List<Recipe> recipeList = new ArrayList<Recipe>();
-                                        List<User> userList = new ArrayList<User>();
-                                        
-		recipeList.addAll(findRecipeByName(attributes));
-                                        recipeList.addAll(findRecipeByIngredients(attributes));
-                                        //userList = getAllUsersController.getAllUsers(); 
-                                        //vyriesit user recognization vJSF
+    public List<Recipe> searchEverywhere(String attributes) {
 
-		if(recipeList.isEmpty()) {
-			
-			//throw new IllegalArgumentException("No recipes found!");
-		}
-		
-		return recipeList;
-	}
+        foundRecipes.clear();
+        List<User> userList = new ArrayList<User>();
 
-	private List<Recipe> findRecipeByName(String inputRecipeName) {
+        foundRecipes.addAll(findRecipeByName(attributes));
+        foundRecipes.addAll(findRecipeByIngredients(attributes));
+        //userList = getAllUsersController.getAllUsers(); 
+        //vyriesit user recognization vJSF
 
-		List<Recipe> recipeList = new ArrayList<Recipe>();
-		List<Recipe> allRecipes = recipeDao.retrieveAllRecipes();
+        if (foundRecipes.isEmpty()) {
+            //throw new IllegalArgumentException("No recipes found!");
+        }
 
-		if (inputRecipeName == null || inputRecipeName.trim().equals("")) {
+        return foundRecipes;
+    }
 
-			throw new IllegalArgumentException("Argument is null");
-		}
+    private List<Recipe> findRecipeByName(String inputRecipeName) {
 
-		for (Recipe recipe : allRecipes) {  
+        List<Recipe> recipeList = new ArrayList<Recipe>();
+        List<Recipe> allRecipes = recipeDao.retrieveAllRecipes();
 
-			if (recipe.getName().contains(inputRecipeName)) {
-				recipeList.add(recipe);
-			}
-		}
+        if (inputRecipeName == null || inputRecipeName.trim().equals("")) {
 
-		return recipeList;
-	}
-        
+            throw new IllegalArgumentException("Argument is null");
+        }
+
+        for (Recipe recipe : allRecipes) {
+
+            if (recipe.getName().contains(inputRecipeName)) {
+                recipeList.add(recipe);
+            }
+        }
+
+        return recipeList;
+    }
+
 //                    private List<User> findUserByNameOrNick(String inputUserName) {
 //
 //		List<User> userList = new ArrayList<User>();
@@ -94,93 +107,92 @@ public class RecipeFinder {
 //
 //		return userList;
 //	}
-        
-                    private Ingredient findIngredientByName(String inputIngredientName) {
+    private Ingredient findIngredientByName(String inputIngredientName) {
 
-		Ingredient wantedIngredient = new Ingredient();
-		List<Ingredient> allIngredients = ingredientDao.retrieveAllIngredients();
+        Ingredient wantedIngredient = new Ingredient();
+        List<Ingredient> allIngredients = ingredientDao.retrieveAllIngredients();
 
-		if (inputIngredientName == null || inputIngredientName.trim().equals("")) {
+        if (inputIngredientName == null || inputIngredientName.trim().equals("")) {
 
-			throw new IllegalArgumentException("Argument is null");
-		}
+            throw new IllegalArgumentException("Argument is null");
+        }
 
-		for (Ingredient ingredient : allIngredients) {  
+        for (Ingredient ingredient : allIngredients) {
 
-			if (ingredient.getName().contains(inputIngredientName)) {
-				wantedIngredient = ingredient;
-			}
-		}
+            if (ingredient.getName().contains(inputIngredientName)) {
+                wantedIngredient = ingredient;
+            }
+        }
 
-		return wantedIngredient;
-	}
+        return wantedIngredient;
+    }
 
-	private List<Recipe> findRecipeByIngredients(String attributes) {
-                                        List<Recipe> recipeByIngredientList =  new ArrayList<Recipe>();
-                                        List<Recipe> allRecipes = recipeDao.retrieveAllRecipes();
+    private List<Recipe> findRecipeByIngredients(String attributes) {
+        List<Recipe> recipeByIngredientList = new ArrayList<Recipe>();
+        List<Recipe> allRecipes = recipeDao.retrieveAllRecipes();
 
-                                        List<String> tokenizedIngredients = tokenizeInputString(attributes);
-                                        List<String> parsedIngredientsToString = new ArrayList<String>();
-                                        
-                                        List<Ingredient> actualIngredientsOfRecipe;
+        List<String> tokenizedIngredients = tokenizeInputString(attributes);
+        List<String> parsedIngredientsToString = new ArrayList<String>();
+
+        List<Ingredient> actualIngredientsOfRecipe;
 //                                        List<Ingredient> actualObligatoryIngredientsOfRecipe = new ArrayList<Ingredient>();;
-                                        
-                                        Map<Recipe, Integer> recipesWithCountedMatchings = new HashMap<Recipe, Integer>();                                        
-                                        //counting matches of input Ingredients with each recipe
-                                        //used Iterator instead of forEach because of removing recipe during cycle
-                                        for (Iterator<Recipe> it = allRecipes.iterator(); it.hasNext();) {
-                                            Recipe recipe = it.next();
-                                            
-                                            actualIngredientsOfRecipe = recipe.getIngredientList();
-                                            Integer count = new Integer(0);
-                                            
-                                            //selecting only obligatory ingredients from current recipe
+
+        Map<Recipe, Integer> recipesWithCountedMatchings = new HashMap<Recipe, Integer>();
+        //counting matches of input Ingredients with each recipe
+        //used Iterator instead of forEach because of removing recipe during cycle
+        for (Iterator<Recipe> it = allRecipes.iterator(); it.hasNext();) {
+            Recipe recipe = it.next();
+
+            actualIngredientsOfRecipe = recipe.getIngredientList();
+            Integer count = new Integer(0);
+
+            //selecting only obligatory ingredients from current recipe
 //                                            for(Ingredient ingredient : actualIngredientsOfRecipe){
 //                                                if (ingredient.isObligatory()) {
 //                                                        actualObligatoryIngredientsOfRecipe.add(ingredient);
 //                                                }
 //                                            }
-                                            
-                                            //converting Ingredient ingredients to String ingredients
-                                            for(Ingredient ingredient : actualIngredientsOfRecipe){ //actualObligatoryIngredientsOfRecipe
-                                                parsedIngredientsToString.add(ingredient.getName());
-                                            }
 
-                                            for (String ingredient: tokenizedIngredients) {
+            //converting Ingredient ingredients to String ingredients
+            for (Ingredient ingredient : actualIngredientsOfRecipe) { //actualObligatoryIngredientsOfRecipe
+                parsedIngredientsToString.add(ingredient.getName());
+            }
 
-                                                if (parsedIngredientsToString.contains(ingredient)) {
-                                                    count = new Integer(count.intValue()+1);
-                                                    
-                                                }  
-                                            }
+            for (String ingredient : tokenizedIngredients) {
 
-                                            if (count.equals(Integer.valueOf(0))) {
-                                                it.remove();
-                                            }
-                                            
-                                            parsedIngredientsToString.clear();
-                                            
-                                            //putting obtained data together
-                                            if(count.intValue()>0){
-                                                recipesWithCountedMatchings.put(recipe, count);
-                                            }
-                                                 
-                                        }
-                                        
-                                        //sorting allRecipes with found matchings according to count of matching ingredients  
-                                        List<Entry<Recipe,Integer>> entries = new ArrayList<Entry<Recipe,Integer>>(recipesWithCountedMatchings.entrySet());
-                                        Collections.sort(entries, new Comparator<Entry<Recipe,Integer>>() {
-                                                    @Override
-                                                    public int compare(Entry<Recipe,Integer> e1, Entry<Recipe,Integer> e2) {
-                                                            return e2.getValue().compareTo(e1.getValue()); // Sorts descending.
-                                                    }
-                                            });
-                                        //inserting found recipes into list 
-                                        for (Entry<Recipe,Integer> entry : entries) {
-                                                    recipeByIngredientList.add(entry.getKey());
-                                        }  
-		return recipeByIngredientList;
-	}
+                if (parsedIngredientsToString.contains(ingredient)) {
+                    count = new Integer(count.intValue() + 1);
+
+                }
+            }
+
+            if (count.equals(Integer.valueOf(0))) {
+                it.remove();
+            }
+
+            parsedIngredientsToString.clear();
+
+            //putting obtained data together
+            if (count.intValue() > 0) {
+                recipesWithCountedMatchings.put(recipe, count);
+            }
+
+        }
+
+        //sorting allRecipes with found matchings according to count of matching ingredients  
+        List<Entry<Recipe, Integer>> entries = new ArrayList<Entry<Recipe, Integer>>(recipesWithCountedMatchings.entrySet());
+        Collections.sort(entries, new Comparator<Entry<Recipe, Integer>>() {
+            @Override
+            public int compare(Entry<Recipe, Integer> e1, Entry<Recipe, Integer> e2) {
+                return e2.getValue().compareTo(e1.getValue()); // Sorts descending.
+            }
+        });
+        //inserting found recipes into list 
+        for (Entry<Recipe, Integer> entry : entries) {
+            recipeByIngredientList.add(entry.getKey());
+        }
+        return recipeByIngredientList;
+    }
 //
 //	public String getAttributes() {
 //		return attributes;
@@ -190,17 +202,18 @@ public class RecipeFinder {
 //		this.attributes = attributes;
 //	}
 //	
-	//TODO : vyspickovat ten tokenizer, osetrit vs. mozne 
-	private List<String> tokenizeInputString(String input) {
-		
-		List<String> tokenList = new ArrayList<String>();
-		String[] tokens = input.split(",\\s+");
-		
-	    for (String token : tokens) {
+    //TODO : vyspickovat ten tokenizer, osetrit vs. mozne 
 
-	    	tokenList.add(token);
-	    }
-		
-		return tokenList;
-	}
+    private List<String> tokenizeInputString(String input) {
+
+        List<String> tokenList = new ArrayList<String>();
+        String[] tokens = input.split(",\\s+");
+
+        for (String token : tokens) {
+
+            tokenList.add(token);
+        }
+
+        return tokenList;
+    }
 }
